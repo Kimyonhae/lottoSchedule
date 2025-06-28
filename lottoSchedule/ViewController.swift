1b38150c1bb1449f46b22dd7258dc4edd58eb132
김용해 <yoho1019@gmail.com>
2025-06-28 14:34:26 +0900

    Initial Commit

---
+++ lottoSchedule/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lottoSchedule
+//
+//  Created by 김용해 on 6/28/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
