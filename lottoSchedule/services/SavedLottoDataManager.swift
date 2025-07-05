bd4c033d0bb848bb7ac2ae6e82565eb3f72a0e31
김용해 <yoho1019@gmail.com>
2025-07-05 21:29:24 +0900

    SavedLottoDataManager 클래스에 로또 생성, 읽기 및 업데이트 기능 추가

--- lottoSchedule/services/SavedLottoDataManager.swift
+++ lottoSchedule/services/SavedLottoDataManager.swift
@@ -5,4 +5,48 @@
 //  Created by 김용해 on 7/5/25.
 //
 
-import Foundation
+import UIKit
+import CoreData
+
+class SavedLottoDataManager {
+    static let shared: SavedLottoDataManager = .init()
+    var savedLottos: [SavedLotto] = [] // 전역으로 사용할 로또 결과들
+    private init() {}
+    private var persistentContainer: NSPersistentContainer? {
+        (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer
+    }
+    
+    // TODO: CREATE Lotto
+    func createLotto(weekltyResult: [Lotto], lottoResultInfo: LottoResultInfo, ranks: [String]) {
+        guard let context = self.persistentContainer?.viewContext else { return }
+        let lotto = SavedLotto(context: context)
+        
+        lotto.id = UUID()
+        lotto.date = lottoResultInfo.date
+        lotto.round = Int32(lottoResultInfo.round)
+        lotto.numbers = weekltyResult.map { $0.numbers } as NSArray
+        lotto.ranks = ranks as NSArray
+        
+        try? context.save()
+    }
+    
+    // TODO: READ Lotto
+    func readLotto() -> [SavedLotto]? {
+        guard let context = persistentContainer?.viewContext else { return nil }
+        let req = SavedLotto.fetchRequest()
+        do {
+            let savedLotto = try context.fetch(req)
+            print("savedLotto count: \(savedLotto.count)")
+            return savedLotto
+        }catch {
+            print("context read Error : \(error)")
+        }
+        
+        return nil
+    }
+    
+    //TODO: 기존 savedLottos 배열을 reload
+    func updateLottos() {
+        self.savedLottos = readLotto() ?? []
+    }
+}
